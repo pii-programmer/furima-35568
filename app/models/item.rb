dac480f016da6b79da6cd8d2047c7abd145c99fe
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-10 21:51:18 +0900

    ActiveHashとItemモデルのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name, length: { maximum: 40 }
     validates :text, length: { maximum: 1000 }
-    validates :category_id
-    validates :status_id
-    validates :shipping_fee_id
-    validates :prefecture_id
-    validates :schedule_delivery_id
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_fee_id
+      validates :prefecture_id
+      validates :schedule_delivery_id
+    end
     validates :price, format:{ with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
     validates :user
     validates :image
@@ -14,4 +16,11 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :status_id
+  belongs_to :shipping_fee_id
+  belongs_to :prefecture_id
+  belongs_to :schedule_delivery_id
 end
