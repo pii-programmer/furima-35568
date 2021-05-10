9e31f6e5887eafc74cdb2b0acc351be44c7b6762
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-10 17:59:29 +0900

    Itemモデルのバリデーション設定/Active Storage追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }
+    validates :text, length: { maximum: 1000 }
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :schedule_delivery_id
+    validates :price, format:{ with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :user
+    validates :image
+  end
+
   belongs_to :user
+  has_one_attached :image
 end
