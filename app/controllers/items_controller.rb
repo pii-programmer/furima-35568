f7d85fc71bb73912dbfb7da2e87fd23e6bb147c7
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-13 20:14:32 +0900

    商品情報編集ページ作成/コントローラーにログイン有無による条件分岐表示を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -28,7 +28,11 @@ class ItemsController < ApplicationController
 
   def update
     item = Item.find(params[:id])
-    item.update(item_params)
+    if item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render :edit
+    end
   end
 
   private
