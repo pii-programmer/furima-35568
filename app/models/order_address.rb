30977c5486874f10563f09b5c2076eced528b1dc
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-16 13:25:11 +0900

    バリデーション/アソシエーション(OrderAddress/Order/Address/User/Item)

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,2 +1,16 @@
-class OrderAddress < ApplicationRecord
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_cord, :prefecture_id, :city, :address, :building, :phone_number, :order_id, :user_id, :item_id
+
+  with_options presence:true do
+    validates :postal_cord,    length: { maximum: 8 }, format: {with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id,  numericality: { other_than: 1 }
+    validates :city,           format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :address,        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]\d[-]\d+\z/ }
+    validates :phone_number,   length: { maximum: 11 }, format: { with: /\A\d{10,11}\z/ }
+    validates :order_id
+    validates :user_id
+    validates :item_id
+  end
+  validates :building,          format: { with: /\A[ぁ-んァ-ヶ一-龥々ー][a-zA-Z\d]+\z/i }
 end
