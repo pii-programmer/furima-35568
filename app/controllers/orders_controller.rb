c851c520362156b475687cd47b11896d25001fc2
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-16 14:07:16 +0900

    ordersコントローラーの作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,23 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    
+  end
+  def new
+    @order_address = OrderAddress.new
+  end
+  def create
+    @order_address = OrderAddress.create(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_cord, :prefecture_id, :city, :address, :building, :phone_number, :order_id).merge(user_id: current_user.id, item_id: item.id)
+  end
+end
