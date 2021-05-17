1fc0bd09589dea1fc469e41da297b0d2e9978b9d
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-17 15:18:29 +0900

    バリデーションを修正OrderAddress

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,13 +6,13 @@ class OrderAddress
     validates :postal_cord,    length: { maximum: 8 }, format: {with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id,  numericality: { other_than: 1 }
     validates :city,           format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :address,        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :address,        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー].+\z/ }
     validates :phone_number,   length: { maximum: 11 }, format: { with: /\A\d{10,11}\z/ }
     validates :order_id
     validates :user_id
     validates :item_id
   end
-  validates :building,          format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :building, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー].+\z/ }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
