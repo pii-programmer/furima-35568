c4ffffb30b78576f57fdc5c1bad3dc088600877f
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-17 21:08:27 +0900

    トークンの値をフォームに追加する記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -17,6 +17,9 @@ const pay = () => {
     Payjp.createToken(card, (status, response) => {
       if (status == 200){
         const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`
+        renderDom.insertAdjacentHTML("afterend", tokenObj);
       }
     });
   });
