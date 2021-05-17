b9e74441842317fbfcc3105a62c4cdde98a90776
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-17 20:28:58 +0900

    クレカ情報をトークン化する記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,24 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_a2f8609fd10357e8d96e54f1");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("テスト！フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_address[number]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
+      cvc: formData.get("order_address[cvc]"),
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200){
+        const token = response.id;
+      }
+    });
   });
 };
 
