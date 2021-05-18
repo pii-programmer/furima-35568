4c4fb5abb1d559d8eac5e5694444ba57656b87c9
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-18 14:45:17 +0900

    [自己修正]サーバーサイドからPAY.JPへトークン情報など送信

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,14 +11,9 @@ class OrdersController < ApplicationController
     @order = Order.create(user_id: current_user.id, item_id: @item.id)
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = "sk_test_a8c735d87c28fa38057f7797"
-      Payjp::Charge.create(
-        amount: order_params[:price],
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @order_address.save
-      redirect_to root_path
+      return redirect_to root_path
     else
       render :index
     end
@@ -27,6 +22,16 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:order_address).permit(:postal_cord, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, order_id: @order.id, token: params[:token])
+  end
+
+  def pay_item
+    @price = Item.find(params[:item_id]).price
+      Payjp.api_key = "sk_test_a8c735d87c28fa38057f7797"
+      Payjp::Charge.create(
+        amount: @price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 
   def move_to_index
