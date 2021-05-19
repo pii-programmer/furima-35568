4fa0285347644795f124fd2e84a1881724c2e1c6
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-19 13:30:34 +0900

    【修正2】編集ページ/購入ページへ直接アクセスの条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -40,7 +40,7 @@ class OrdersController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:item_id])
-    if (current_user.id == @item.user_id) && @item.order.present?
+    if (current_user.id == @item.user_id) || @item.order.present?
       redirect_to root_path
     end
   end
