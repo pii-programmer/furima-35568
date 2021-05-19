e96adad808c774d9e1dfb4d5d2ec40e1b0b3159d
pii-programmer <82761385+pii-programmer@users.noreply.github.com>
2021-05-19 09:43:35 +0900

    [条件分岐] 売却済み商品の購入ページURLへ直接アクセスするとトップページへ遷移する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,7 +36,7 @@ class OrdersController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:item_id])
-    if current_user.id == @item.user_id
+    if user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
   end
